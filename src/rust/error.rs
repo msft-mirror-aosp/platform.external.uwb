50c4f7023e18549dfa1148a3db7706e89909319e
Joel Galenson <jgalenson@google.com>
2021-12-20 15:33:42 -0800

    Migrate to thiserror.
    Bug: 197341298
    Test: libuwb_uci_rust_tests
    Test: See some errors in logcat
    Change-Id: I06346803668a689381c543b76b3ccca4f3e911c1

---
+++ src/rust/error.rs
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use android_hardware_uwb::aidl::android::hardware::uwb::UwbStatus::UwbStatus;
+use tokio::sync::mpsc;
+
+#[derive(Debug, thiserror::Error)]
+pub enum UwbErr {
+    #[error("UWBStatus error: {0:?}")]
+    Status(UwbStatus),
+    #[error("Binder error: {0}")]
+    Binder(#[from] binder::Status),
+    #[error("JNI error: {0}")]
+    Jni(#[from] jni::errors::Error),
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("SendError for JNICommand: {0}")]
+    SendJNICommand(#[from] mpsc::error::SendError<crate::uci::JNICommand>),
+    #[error("SendError for HALResponse: {0}")]
+    SendHALResponse(#[from] mpsc::error::SendError<crate::uci::HALResponse>),
+    #[error("Exit")]
+    Exit,
+    #[error("Unknown error")]
+    Undefined,
+}
+
+impl UwbErr {
+    pub fn failed() -> Self {
+        UwbErr::Status(UwbStatus::FAILED)
+    }
+
+    pub fn refused() -> Self {
+        UwbErr::Status(UwbStatus::REFUSED)
+    }
+}
