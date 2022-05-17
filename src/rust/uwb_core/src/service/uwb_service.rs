d170238353e88b29dd938afe878c076416c0ca24
Chih-Yu Huang <akahuang@google.com>
2022-05-17 11:04:46 +0900

    uwb_core: add test cases for UwbService's session notification
    This CL adds a test case to verify the flow that UwbService initiates
    a ranging session, gets ranging data, and then deinitiate the session.
    
    Bug: 228395837
    Test: cargo test
    Test: cargo clippy
    Change-Id: I6aaf7c00f31326e44fbb6cbe8b4a49940c3b8656

--- src/rust/uwb_core/src/service/uwb_service.rs
+++ src/rust/uwb_core/src/service/uwb_service.rs
@@ -419,11 +419,13 @@ type ResponseSender = oneshot::Sender<Result<Response>>;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::session::session_manager::test_utils::generate_params;
+    use crate::session::session_manager::test_utils::{
+        generate_params, range_data_notf, session_range_data, session_status_notf,
+    };
     use crate::uci::error::StatusCode;
     use crate::uci::mock_uci_manager::MockUciManager;
     use crate::uci::notification::UciNotification;
-    use crate::uci::params::power_stats_eq;
+    use crate::uci::params::{power_stats_eq, SessionState, SetAppConfigResponse};
 
     #[tokio::test]
     async fn test_open_close_uci() {
@@ -439,7 +441,75 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_api_without_enabled() {
+    async fn test_session_e2e() {
+        let session_id = 0x123;
+        let session_type = SessionType::FiraRangingSession;
+        let params = generate_params();
+        let tlvs = params.generate_tlvs();
+        let range_data = session_range_data(session_id);
+
+        let mut uci_manager = MockUciManager::new();
+        uci_manager.expect_open_hal(vec![], Ok(()));
+        uci_manager.expect_session_init(
+            session_id,
+            session_type,
+            vec![session_status_notf(session_id, SessionState::SessionStateInit)],
+            Ok(()),
+        );
+        uci_manager.expect_session_set_app_config(
+            session_id,
+            tlvs,
+            vec![session_status_notf(session_id, SessionState::SessionStateIdle)],
+            Ok(SetAppConfigResponse { status: StatusCode::UciStatusOk, config_status: vec![] }),
+        );
+        uci_manager.expect_range_start(
+            session_id,
+            vec![
+                session_status_notf(session_id, SessionState::SessionStateActive),
+                range_data_notf(range_data.clone()),
+            ],
+            Ok(()),
+        );
+        uci_manager.expect_range_stop(
+            session_id,
+            vec![session_status_notf(session_id, SessionState::SessionStateIdle)],
+            Ok(()),
+        );
+        uci_manager.expect_session_deinit(
+            session_id,
+            vec![session_status_notf(session_id, SessionState::SessionStateDeinit)],
+            Ok(()),
+        );
+
+        let (notf_sender, mut notf_receiver) = mpsc::unbounded_channel();
+        let mut service = UwbService::new(notf_sender, uci_manager.clone());
+        service.enable().await.unwrap();
+
+        // Initialize a normal session.
+        let result = service.init_session(session_id, session_type, params.clone()).await;
+        assert!(result.is_ok());
+
+        // Start the ranging process, and should receive the range data.
+        let result = service.start_ranging(session_id).await;
+        assert!(result.is_ok());
+        let session_notf = notf_receiver.recv().await.unwrap();
+        assert_eq!(session_notf, UwbNotification::RangeDataReceived { session_id, range_data });
+
+        // Stop the ranging process.
+        let result = service.stop_ranging(session_id).await;
+        assert!(result.is_ok());
+
+        // Deinitialize the session, and should receive the deinitialized notification.
+        let result = service.deinit_session(session_id).await;
+        assert!(result.is_ok());
+        let session_notf = notf_receiver.recv().await.unwrap();
+        assert_eq!(session_notf, UwbNotification::SessionDeinited { session_id });
+
+        assert!(uci_manager.wait_expected_calls_done().await);
+    }
+
+    #[tokio::test]
+    async fn test_session_api_without_enabled() {
         let session_id = 0x123;
         let session_type = SessionType::FiraRangingSession;
         let params = generate_params();
