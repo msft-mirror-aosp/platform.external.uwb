f7282fee47ce95b0733733bf9e2923116a8d5cd6
Chih-Yu Huang <akahuang@google.com>
2022-05-26 10:26:38 +0900

    uwb_core: add the document for public components
    This CL adds the document for most of the public components, and
    removes the lint annotation that allows missing the documents at the
    root of the library. After this CL merged, we should fill the document
    when adding new public components.
    
    Bug: 233319010
    Test: cargo test
    Change-Id: I1802567ef3d69c5968e0f2f1e4ee482b6eeb28c5

--- src/rust/uwb_core/src/params/uci_packets.rs
+++ src/rust/uwb_core/src/params/uci_packets.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Define the structs and enums for the parameters or responses of the UciManager's methods.
-//! Most of them are re-exported uwb_uci_packets's structure.
+//! This module defines the parameters or responses of the UciManager's methods. Most of them are
+//! re-exported from the uwb_uci_packets crate.
 
 use std::collections::{hash_map::RandomState, HashMap};
 use std::iter::FromIterator;
@@ -27,9 +27,13 @@ pub use uwb_uci_packets::{
     ShortAddressTwoWayRangingMeasurement, StatusCode, UpdateMulticastListAction,
 };
 
+/// The type of the session identifier.
 pub type SessionId = u32;
+/// The type of the sub-session identifier.
 pub type SubSessionId = u32;
 
+/// Compare if two AppConfigTlv array are equal. Convert the array to HashMap before comparing
+/// because the order of TLV elements doesn't matter.
 #[allow(dead_code)]
 pub fn app_config_tlvs_eq(a: &[AppConfigTlv], b: &[AppConfigTlv]) -> bool {
     app_config_tlvs_to_map(a) == app_config_tlvs_to_map(b)
@@ -41,6 +45,8 @@ fn app_config_tlvs_to_map(
     HashMap::from_iter(tlvs.iter().map(|config| (config.cfg_id, &config.v)))
 }
 
+/// Compare if two DeviceConfigTlv array are equal. Convert the array to HashMap before comparing
+/// because the order of TLV elements doesn't matter.
 #[allow(dead_code)]
 pub fn device_config_tlvs_eq(a: &[DeviceConfigTlv], b: &[DeviceConfigTlv]) -> bool {
     device_config_tlvs_to_map(a) == device_config_tlvs_to_map(b)
@@ -52,22 +58,30 @@ fn device_config_tlvs_to_map(
     HashMap::from_iter(tlvs.iter().map(|config| (config.cfg_id, &config.v)))
 }
 
+/// The response of the UciManager::core_set_config() method.
 #[derive(Debug, Clone, PartialEq)]
 pub struct CoreSetConfigResponse {
+    /// The status code of the response.
     pub status: StatusCode,
+    /// The status of each config TLV.
     pub config_status: Vec<DeviceConfigStatus>,
 }
 
+/// The response of the UciManager::session_set_app_config() method.
 #[derive(Debug, Clone, PartialEq)]
 pub struct SetAppConfigResponse {
+    /// The status code of the response.
     pub status: StatusCode,
+    /// The status of each config TLV.
     pub config_status: Vec<AppConfigStatus>,
 }
 
+/// The country code struct that contains 2 uppercase ASCII characters.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CountryCode([u8; 2]);
 
 impl CountryCode {
+    /// Create a CountryCode instance.
     pub fn new(code: &[u8; 2]) -> Option<Self> {
         if !code[0].is_ascii_uppercase() || !code[1].is_ascii_uppercase() {
             None
@@ -83,18 +97,28 @@ impl From<CountryCode> for [u8; 2] {
     }
 }
 
+/// The response of the UciManager::core_get_device_info() method.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct GetDeviceInfoResponse {
+    /// The UCI version.
     pub uci_version: u16,
+    /// The MAC version.
     pub mac_version: u16,
+    /// The physical version.
     pub phy_version: u16,
+    /// The UCI test version.
     pub uci_test_version: u16,
+    /// The vendor spec info.
     pub vendor_spec_info: Vec<u8>,
 }
 
+/// The raw UCI message for the vendor commands.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RawVendorMessage {
+    /// The group id of the message.
     pub gid: u32,
+    /// The opcode of the message.
     pub oid: u32,
+    /// The payload of the message.
     pub payload: Vec<u8>,
 }
