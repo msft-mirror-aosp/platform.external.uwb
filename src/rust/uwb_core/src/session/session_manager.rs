f6b8908a080cc359e8a0a11ada390c20fde674bd
Ayush Jain <ayushjain@google.com>
2024-03-27 22:08:55 +0000

    Support Multicast NTF V1/V2 packet formats.
    Fira 1.x had defined the SESSION_UPDATE_CONTROLLER_MULTICAST_LIST_NTF to
    have a StatusList[] with 3 fields (MacAddress, SubSessionId, Status -
    size of 7 bytes).
    
    Fira 2.0 changed the definition of StatusList[] with 2 fields
    (MacAddress, Status - size of 3 bytes).
    
    This CL does the packet conversions with an additional UCI version
    parameter given to the try_into() conversion methods (as a tuple).
    
    Test: make com.android.uwb
    Test: atest libuwb_uci_jni_rust_tests libuwb_core_tests libuwb_uci_packet_tests
    Test: https://paste.googleplex.com/6207883791826944
    Bug: 322549007
    Change-Id: I6b406c5b157a3a7b639ba492e4e5bcb3b643946d

--- src/rust/uwb_core/src/session/session_manager.rs
+++ src/rust/uwb_core/src/session/session_manager.rs
@@ -336,13 +336,16 @@ impl<T: UciManager> SessionManagerActor<T> {
             UciSessionNotification::UpdateControllerMulticastList {
                 session_token,
                 remaining_multicast_list_size: _,
-                status_list,
+                status_list_v1,
+                status_list_v2: _,
             } => match self.active_sessions.get_mut(&session_token) {
-                Some(session) => session.on_controller_multicast_list_udpated(status_list),
+                // TODO: Use status_list_v2 when it's not empty (can convert it into
+                // status_list_v1, since that is a super-set).
+                Some(session) => session.on_controller_multicast_list_udpated(status_list_v1),
                 None => {
                     warn!(
                         "Received the notification of the unknown Session {}: {:?}",
-                        session_token, status_list
+                        session_token, status_list_v1,
                     );
                 }
             },
@@ -584,7 +587,7 @@ mod tests {
 
     use crate::params::ccc_started_app_config_params::CccStartedAppConfigParams;
     use crate::params::uci_packets::{
-        AppConfigTlv, AppConfigTlvType, ControleeStatus, Controlees, MulticastUpdateStatusCode,
+        AppConfigTlv, AppConfigTlvType, ControleeStatusV1, Controlees, MulticastUpdateStatusCode,
         ReasonCode, SetAppConfigResponse, StatusCode,
     };
     use crate::params::utils::{u32_to_bytes, u64_to_bytes, u8_to_bytes};
@@ -813,11 +816,12 @@ mod tests {
                     UciSessionNotification::UpdateControllerMulticastList {
                         session_token: session_id,
                         remaining_multicast_list_size: 1,
-                        status_list: vec![ControleeStatus {
+                        status_list_v1: vec![ControleeStatusV1 {
                             mac_address: [0x34, 0x12],
                             subsession_id: 0x24,
                             status: MulticastUpdateStatusCode::StatusOkMulticastListUpdate,
                         }],
+                        status_list_v2: vec![],
                     },
                 )];
                 uci_manager.expect_session_init(
