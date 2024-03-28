fe4ccb7efe94806a93ff60fbf4f9388d6713785b
Dajung Lee <dajunglee@google.com>
2024-03-28 21:01:10 +0000

    Revert "Support Multicast NTF V1/V2 packet formats."
    Revert submission 3018363-uwb_multicast_ntf
    
    Reason for revert: b/331836038
    
    Reverted changes: /q/submissionid:3018363-uwb_multicast_ntf
    
    Change-Id: I29b995c1b4e248f49b700987d3546f47261969d2

--- src/rust/uwb_core/src/uci/message.rs
+++ src/rust/uwb_core/src/uci/message.rs
@@ -26,17 +26,15 @@ pub(super) enum UciMessage {
     Notification(UciNotification),
 }
 
-impl TryFrom<(uwb_uci_packets::UciControlPacket, u8)> for UciMessage {
+impl TryFrom<uwb_uci_packets::UciControlPacket> for UciMessage {
     type Error = Error;
-    fn try_from(pair: (uwb_uci_packets::UciControlPacket, u8)) -> Result<Self, Self::Error> {
-        let packet = pair.0;
-        let uci_fira_major_ver = pair.1;
+    fn try_from(packet: uwb_uci_packets::UciControlPacket) -> Result<Self, Self::Error> {
         match packet.specialize() {
             uwb_uci_packets::UciControlPacketChild::UciResponse(evt) => {
                 Ok(UciMessage::Response(evt.try_into()?))
             }
             uwb_uci_packets::UciControlPacketChild::UciNotification(evt) => {
-                Ok(UciMessage::Notification((evt, uci_fira_major_ver).try_into()?))
+                Ok(UciMessage::Notification(evt.try_into()?))
             }
             _ => {
                 error!("Unknown packet for converting to UciMessage: {:?}", packet);
